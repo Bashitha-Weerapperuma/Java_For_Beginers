ebff33d299fddc5fe7467d4f53374d735c5d5402
bashitha weerapperuma <beweerapperuma@gmail.com>
2024-05-31 19:10:11 +0530

    shortifelse

---
+++ src/short_ifElse.java
@@ -0,0 +1,8 @@
+public class short_ifElse {
+    public static void main(String[] args) {
+        int marks = 80;
+        // condition ? True : False
+        String result = (marks >50) ? "Pass" : "Fail";
+        System.out.println(result);
+    }
+}
