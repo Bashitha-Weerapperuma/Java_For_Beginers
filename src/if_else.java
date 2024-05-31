f067a4136e894ea84d9312dbc598a168df1e4318
bashitha weerapperuma <beweerapperuma@gmail.com>
2024-05-31 19:05:17 +0530

    if else

---
+++ src/if_else.java
@@ -0,0 +1,10 @@
+public class if_else {
+    public static void main(String[] args) {
+        int time= 6;
+
+        if(time<12)
+            System.out.println("Morning");
+        else
+            System.out.println("Night");
+    }
+}
